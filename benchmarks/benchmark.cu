a117c5f9c997fa2257d7ba0d6025c5c7c5bc5c56
Jacob Teddy <tetraphobia@wyvrn.dev>
2026-02-10 10:30:03 -0500

    feat: nvbench submodule

---
+++ benchmarks/benchmark.cu
@@ -0,0 +1,6 @@
+#include "../src/shared/matrix_utils.h"
+#include "../src/kernels/cpu_naive.h"
+#include "../src/kernels/cuda_naive.cuh"
+#include "../src/kernels/cuda_rf_block.cuh"
+#include "../src/kernels/cuda_shared.cuh"
+
